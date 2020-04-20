e8fc0d89ca291904f17741e5b5dc8da3bfb54b70
PHAGUN JAIN <53911145+PHAGUN-JAIN@users.noreply.github.com>
2020-04-20 14:57:01 +0530

    derived classes from two base classes

--- inheritance/MultipleInheritence.cpp
+++ inheritance/MultipleInheritence.cpp
@@ -18,5 +18,18 @@ void Output::print (int i) {
   cout << i << '\n';
 }
 
-
-
+                                                  //first derived class
+class Rectangle: public Polygon, public Output {
+  public:
+    Rectangle (int a, int b) : Polygon(a,b) {}
+    int area ()
+      { return width*height; }
+};
+                                                   //second derived class
+class Triangle: public Polygon, public Output {
+  public:
+    Triangle (int a, int b) : Polygon(a,b) {}
+    int area ()
+      { return width*height/2; }
+};
+  
